c31939e678a0679e5601da505db11e023cff3751
Charlotte Moore <mooc123@gmail.com>
2017-03-29 09:25:09 -0600

    adds search form on root, creates feature test for search form

--- spec/features/user_visits_root_spec.rb
+++ spec/features/user_visits_root_spec.rb
@@ -1,9 +1,18 @@
 require "rails_helper"
 
 describe "when user visits root" do
-  it "they see a search bar" do
+  it "they see a welcome message" do
     visit root_path
 
     expect(page).to have_content("Welcome")
+  end
+
+  it "they are able to fill out a search bar and click submit" do
+    visit root_path
+
+    fill_in("search", :with => "80203")
+    click_on("Search")
+
+    expect(current_path).to eq(search_path)
   end
 end
